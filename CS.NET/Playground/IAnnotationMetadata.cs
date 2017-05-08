89b6420b61589aa8ca1b5b95e393849c3b9115c9
Plexon21 <plexon21@bluewin.ch>
2017-05-08 17:47:45 +0200

    Added Playground for MEF

---
+++ CS.NET/Playground/IAnnotationMetadata.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playground
+{
+    public interface IAnnotationMetadata
+    {
+        string Name { get; }
+
+        [DefaultValue(1)]
+        int Version { get; }
+    }
+}
--- CS.NET/Playground/Program.cs
+++ CS.NET/Playground/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,9 +10,15 @@ using System.Threading.Tasks;
 namespace Playground
 {
     public class Program
-    {
+    {   
         static void Main(string[] args)
         {
+            AnnotationHandler handler = new AnnotationHandler();
+            foreach (var annot in handler.annotations) {
+                Console.WriteLine(annot.Metadata.Name + " Version:" + annot.Metadata.Version);
+                Console.WriteLine(annot.Value.show());
+            }
+            Console.ReadKey();
         }
     }
 }
